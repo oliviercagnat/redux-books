f94050e5f3576952ed5fa688c9baf9d0773d665d
oliviercagnat <oliviercagnat@gmail.com>
2022-08-26 12:16:26 +0900

    init app

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer text-white bg-secondary mt-5">
+        <div className="container p-3">
+            <span>&copy; Olivier Cagnat</span>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <header>
+        <div className="d-flex flex-column flex-md-row p-3 border-bottom bg-secondary text-white">
+            <h4 className="mr-md-auto">
+                <a href="/" className="text-decoration-none text-white">BOOKS</a>
+            </h4>
+        </div>
+
+        {/* Menu */}
+    </header>
+  )
+}
+
+export default NavBar
